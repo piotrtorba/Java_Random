71ad4510a865dd01d1a9d612cfdc6803709b4879
root <torba.piotr@gmail.com>
2023-05-19 21:32:07 +0200

    --> Final <--

--- src/YT_JNSP/ProjectComputer/Main.java
+++ src/YT_JNSP/ProjectComputer/Main.java
@@ -1,5 +1,6 @@
 package YT_JNSP.ProjectComputer;
 
+import YT_JNSP.ProjectComputer.Drive.Drive;
 import YT_JNSP.ProjectComputer.Drive.HDDdrive;
 import YT_JNSP.ProjectComputer.Drive.SSDdrive;
 import YT_JNSP.ProjectComputer.File.File;
@@ -15,8 +16,8 @@ public class Main {
         // Shortes way to create new object 'Computer' with its' arguments (display & drive):
         // new Computer(new Display(), new HDDdrive());
         Display display = new Display();
-        //HDDdrive drive = new HDDdrive();
-        SSDdrive drive = new SSDdrive();
+        Drive drive = new HDDdrive();
+        //SSDdrive drive = new SSDdrive();
         Computer Desktop_1 = new Computer(display, drive);
 
         /*drive.addFile(new File("testFile.txt"));
@@ -30,9 +31,9 @@ public class Main {
        Desktop_1.addUSBDevice(memoryStick);
        memoryStick.getName();
 
-       /*Desktop_1.removerUSBDevice(mouse);
+       Desktop_1.removerUSBDevice(mouse);
        memoryStick.eject();
-       Desktop_1.removerUSBDevice(memoryStick);*/
+       Desktop_1.removerUSBDevice(memoryStick);
 
         GIFImage gifImage1 = new GIFImage("nazwa.gif", 100);
         JPGImage jpgImage1 = new JPGImage("nazwa.jpg", 255, 11);
@@ -42,7 +43,7 @@ public class Main {
         drive.addFile(jpgImage1);
         drive.addFile(mp3File1);
         drive.listFiles();
-        File file = drive.findFile("mp3File1");
+        File file = drive.findFile("nazwa.mp3");
         System.out.println(file.getSize());
 
         /*gifImage1.getName();
